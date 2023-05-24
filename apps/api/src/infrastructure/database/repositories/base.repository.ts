2dd3debb8f6762e38dbd5cbe4f8dbd66d988880d
Yahia <yahia.lamri@outlook.fr>
2023-05-25 01:04:11 +0200

    chore(architecture): create base repository

---
+++ apps/api/src/infrastructure/database/repositories/base.repository.ts
@@ -0,0 +1,22 @@
+import { DataSource, EntityManager, EntityTarget, ObjectLiteral, QueryRunner } from "typeorm";
+
+
+export class BaseRepository<Entity extends ObjectLiteral> {
+  readonly manager: EntityManager;
+  readonly queryRunner?: QueryRunner;
+  readonly entity: EntityTarget<Entity>;
+
+  constructor(connection: DataSource, entity: EntityTarget<Entity>) {
+    this.queryRunner = connection.createQueryRunner();
+    this.manager = this.queryRunner.manager;
+    this.entity = entity;
+  }
+
+  save(entity: Entity): Promise<Entity> {
+    return this.manager.save(entity);
+  }
+
+  find(): Promise<Entity[]> {
+    return this.manager.find(this.entity);
+  }
+}
--- apps/api/src/infrastructure/database/repositories/plugin.repository.ts
+++ apps/api/src/infrastructure/database/repositories/plugin.repository.ts
@@ -1,28 +1,26 @@
 import {  Injectable } from "@nestjs/common";
 import { randomStringGenerator } from "@nestjs/common/utils/random-string-generator.util";
 import { InjectDataSource } from "@nestjs/typeorm";
-import { DataSource, Repository } from "typeorm";
+import { DataSource, EntitySchema, Repository } from "typeorm";
 import { Plugin } from "../entities/plugin.entity";
 
 import { PluginRepository } from "../../../domain/interfaces/repository/plugin.repository";
+import { BaseRepository } from "./base.repository";
 
 
 @Injectable()
-export class PluginRepositoryImpl implements PluginRepository {
-  db: Repository<Plugin>;
+export class PluginRepositoryImpl extends BaseRepository<Plugin> implements PluginRepository {
 
-  constructor(
-    @InjectDataSource() private readonly connection: DataSource,
-  ) {
-    this.db = connection.getRepository(Plugin);
+  constructor(@InjectDataSource() connection: DataSource) {
+    super(connection, Plugin);
   }
 
   createPlugin(plugin: Plugin): Promise<Plugin> {
-    return this.db.save({ ...plugin, name: randomStringGenerator() });
+    return this.save({ ...plugin, name: randomStringGenerator() });
   }
 
   getPlugins(): Promise<Plugin[]> {
-    return this.db.manager.find(Plugin);
+    return this.find();
   }
 }
 
