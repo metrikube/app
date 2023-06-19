92a9c72d9d59402591d426063a223ba9fe60bd0d
Yahia <yahia.lamri@outlook.fr>
2023-06-19 16:35:40 +0200

    fix(database): change db provider

--- apps/api/src/infrastructure/config/typeorm/database.module.ts
+++ apps/api/src/infrastructure/config/typeorm/database.module.ts
@@ -1,31 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-export const getTypeOrmModuleOptions = (config: EnvironmentConfigService): TypeOrmModuleOptions =>
-  ({
-    type: 'mariadb',
-    host: config.getDatabaseHost(),
-    port: config.getDatabasePort(),
-    username: config.getDatabaseUser(),
-    password: config.getDatabasePassword(),
-    database: config.getDatabaseName(),
-    // entities: [__dirname + './../../**/*.entity{.ts,.js}'],
-    entities: [__dirname + '/../../entities/*.entity{.ts,.js}'],
-    synchronize: false,
-    autoLoadEntities: true,
-    migrationsRun: true,
-    migrations: [__dirname + '/../../**/migrations/**/*{.ts,.js}'],
-    cli: {
-      migrationsDir: 'src/migrations',
-    },
-  } as TypeOrmModuleOptions);
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db',
-      // entities: [Plugin],
       entities: [__dirname + '/../../database/entities/*.entity{.ts,.js}'],
       synchronize: true,
     }),
