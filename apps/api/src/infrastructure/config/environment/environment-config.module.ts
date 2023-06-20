df6d472bb05bdf8f03e80487a1f2797d47e312af
Yahia <yahia.lamri@outlook.fr>
2023-06-20 17:07:12 +0200

    chore(logger): init logger middleware and exception filter

--- apps/api/src/infrastructure/config/environment/environment-config.module.ts
+++ apps/api/src/infrastructure/config/environment/environment-config.module.ts
@@ -8,13 +8,13 @@ import { EnvironmentConfigService } from './environment-config.service';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: `./env/.env.${process.env.NODE_ENV}`,
-      isGlobal: true,
-      validate,
-    }),
+    // ConfigModule.forRoot({
+    //   envFilePath: `./env/.env.${process.env.NODE_ENV}`,
+    //   isGlobal: true,
+    //   validate,
+    // }),
   ],
   providers: [EnvironmentConfigService],
-  exports: [EnvironmentConfigService],
+  exports: [EnvironmentConfigService]
 })
 export class EnvironmentConfigModule {}
--- apps/api/src/presenter/controllers/app.controller.ts
+++ apps/api/src/presenter/controllers/app.controller.ts
@@ -3,7 +3,7 @@ import { Controller, Get, Inject, Post } from '@nestjs/common';
 import { PluginUseCaseInterface } from '../../domain/interfaces/use-cases/plugin.use-case.interface';
 import { Plugin } from '../../infrastructure/database/entities/plugin.entity';
 
-@Controller()
+@Controller('/')
 export class AppController {
   constructor(@Inject('PLUGIN_USE_CASE') private readonly pluginUseCase: PluginUseCaseInterface) {}
 
