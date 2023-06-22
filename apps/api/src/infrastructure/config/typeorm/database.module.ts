b6d96725a67ca1a89b9b0dd1ac42e917dc682807
Yahia <yahia.lamri@outlook.fr>
2023-06-22 14:40:49 +0200

    feat(db): change sqlite3 > better-sqlite3 for more db performance

--- apps/api/src/infrastructure/config/typeorm/database.module.ts
+++ apps/api/src/infrastructure/config/typeorm/database.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { AlertEntity } from '../../database/entities/alert.entity';
 import { CredentialEntity } from '../../database/entities/credential.entity';
 import { PluginEntity } from '../../database/entities/plugin.entity';
+import { WidgetEntity } from '../../database/entities/widget.entity';
 
-const entities = [PluginEntity, CredentialEntity];
+const entities = [PluginEntity, CredentialEntity, WidgetEntity, AlertEntity];
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db',
+      type: 'better-sqlite3',
+      database: 'data/db',
       entities,
       synchronize: true,
       autoLoadEntities: true
