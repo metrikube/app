2e9159c924f5187808103825dc5e0e0750a90339
Yahia <yahia.lamri@outlook.fr>
2023-06-22 14:39:35 +0200

    format(module)

--- apps/api/src/presenter/controllers/app.controller.ts
+++ apps/api/src/presenter/controllers/app.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Inject, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
 
 import { PluginUseCaseInterface } from '../../domain/interfaces/use-cases/plugin.use-case.interface';
 import { Plugin } from '../../domain/models/plugin.model';
@@ -9,6 +10,7 @@ export class AppController {
   constructor(@Inject('PLUGIN_USE_CASE') private readonly pluginUseCase: PluginUseCaseInterface) {}
 
   @Get()
+  @ApiProperty({})
   getHello(): Promise<PluginEntity[]> {
     return this.pluginUseCase.getPlugins();
   }
