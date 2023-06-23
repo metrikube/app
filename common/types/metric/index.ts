8236f1a2dbb0d828142ebcf261457c3f19d6b2c7
Yahia <yahia.lamri@outlook.fr>
2023-06-23 16:09:10 +0200

    feat(types): create types structure for Generic plugins responses

---
+++ common/types/metric/index.ts
@@ -0,0 +1,11 @@
+export type MetricType =
+  | 'api_endpoint_health_check'
+  | 'aws_bucket_single_instance'
+  | 'aws_bucket_multiple_instances'
+  | 'aws_ec2_multiple_instances_usage'
+  | 'aws_ec2_single_instance_usage'
+  | 'github_last_pr'
+  | 'database_queries'
+  | 'database_size'
+  | 'database_connections'
+  | 'database_slow_queries';
