9093adab8e1113bfc55d25512a226180dc160051
Théotim HERIN <theotim.herin@netsach.com>
2023-06-23 18:28:35 +0200

    resolve conflicts after change

--- apps/api/src/domain/models/credential.model.ts
+++ apps/api/src/domain/models/credential.model.ts
@@ -1,16 +1,6 @@
 import { Plugin } from './plugin.model';
 import { CredentialType, GenericCredentialType } from '@metrikube/common';
 
-export type CredentialType = {
-  readonly apiKey: { apiKey: string };
-  readonly userPassword: { username: string; password: string };
-  readonly dbConnection: { dbName: string; dbUser: string; dbPassword: string; dbHost: string; dbPort: number };
-  readonly apiEndpoint: { apiEndpoint: string };
-  readonly passport: { passportConfig: unknown; strategyName: string };
-  readonly plugin: Plugin;
-
-};
-
 export type Credential = {
   readonly type: CredentialType;
   readonly value: GenericCredentialType | string;
--- apps/api/src/presenter/controllers.module.ts
+++ apps/api/src/presenter/controllers.module.ts
@@ -6,20 +6,17 @@ import { Module } from '@nestjs/common';
 import { UseCaseModule } from '../application/use-case.module';
 import { PluginUseCase } from '../application/use-cases/plugin/plugin.use-case';
 import { CredentialUseCase } from '../application/use-cases/credential.use-case';
-
 import { CredentialRepositoryImpl } from '../infrastructure/database/repositories/credential.repository';
 import { PluginRepositoryImpl } from '../infrastructure/database/repositories/plugin.repository';
 import { AppController } from './controllers/app.controller';
 import { PluginController } from './controllers/plugin.controller';
+
 
 @Module({
   imports: [UseCaseModule],
   controllers: [AppController, PluginController],
   providers: [
     { provide: 'PLUGIN_USE_CASE', useClass: PluginUseCase },
-    { provide: 'PLUGIN_REPOSITORY', useClass: PluginRepositoryImpl },
-    { provide: 'COST_EXPLORER_SERVICE', useClass: CostExplorerService },
-    { provide: 'EC2_SERVICE', useClass: EC2Service },
     { provide: 'AWS_PLUGIN', useClass: AWSService },
     { provide: 'API_MONITORING', useClass: ApiMonitoringService },
     { provide: 'CREDENTIAL_USE_CASE', useClass: CredentialUseCase },
