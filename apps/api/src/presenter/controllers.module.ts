28a4a8ef933bf477dc081a54885b1f9061dc5623
Yahia <yahia.lamri@outlook.fr>
2023-06-23 16:12:06 +0200

    feat(api-hc-plugin): create test route with service

--- apps/api/src/presenter/controllers.module.ts
+++ apps/api/src/presenter/controllers.module.ts
@@ -1,3 +1,4 @@
+import { ApiMonitoringService } from '@metrikube/api-monitoring';
 import { AWSService } from '@metrikube/aws-plugin';
 
 import { Module } from '@nestjs/common';
@@ -7,14 +8,16 @@ import { PluginUseCase } from '../application/use-cases/plugin/plugin.use-case';
 import { CredentialRepositoryImpl } from '../infrastructure/database/repositories/credential.repository';
 import { PluginRepositoryImpl } from '../infrastructure/database/repositories/plugin.repository';
 import { AppController } from './controllers/app.controller';
+import { PluginController } from './controllers/plugin.controller';
 
 @Module({
   imports: [UseCaseModule],
-  controllers: [AppController],
+  controllers: [AppController, PluginController],
   providers: [
     { provide: 'PLUGIN_USE_CASE', useClass: PluginUseCase },
     { provide: 'PLUGIN_REPOSITORY', useClass: PluginRepositoryImpl },
     { provide: 'AWS_PLUGIN', useClass: AWSService },
+    { provide: 'API_MONITORING', useClass: ApiMonitoringService },
     { provide: 'CREDENTIAL_REPOSITORY', useClass: CredentialRepositoryImpl }
   ]
 })
---
+++ apps/api/src/presenter/controllers/plugin.controller.ts
@@ -0,0 +1,22 @@
+import { ApiMonitoringService } from '@metrikube/api-monitoring';
+import { MetricType, PluginResult } from '@metrikube/common';
+
+import { Controller, Get, Inject, Param } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('plugin')
+@Controller('plugin')
+export class PluginController {
+  constructor(@Inject('API_MONITORING') private readonly apiMonitoring: ApiMonitoringService) {}
+
+  @Get('/:id/:metric')
+  @ApiOperation({ summary: 'Get plugin data' })
+  getPluginData(@Param('id') pluginId: string, @Param('metric') metric: MetricType): PluginResult<typeof metric> {
+    // Todo : get the data and credentials for this plugin from the database
+    // Todo : call the plugin service to get the data
+    // Todo : return the data
+    return this.apiMonitoring.apiHealthCheck({
+      apiEndpoint: 'https://jsonplaceholder.typicode.com/users'
+    });
+  }
+}
