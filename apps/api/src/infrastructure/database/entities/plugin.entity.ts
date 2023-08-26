b950f504f4bfab7a6497489fd97f38818806d04f
Yahia <yahia.lamri@outlook.fr>
2023-08-27 01:40:05 +0200

    feat(migrations): initialize migrations and create default seed

--- apps/api/src/infrastructure/database/entities/plugin.entity.ts
+++ apps/api/src/infrastructure/database/entities/plugin.entity.ts
@@ -1,7 +1,5 @@
 import { Column, CreateDateColumn, Entity, Generated, JoinColumn, OneToMany, PrimaryColumn } from 'typeorm';
 
-import { ApiProperty } from '@nestjs/swagger';
-
 import { MetricEntity } from './metric.entity';
 import { PluginToMetricEntity } from './plugin_to_metric.entity';
 
@@ -16,63 +14,37 @@ const pluginInstructionExample = `
 export class PluginEntity {
   @Generated('uuid')
   @PrimaryColumn({ type: 'uuid' })
-  @ApiProperty({ name: 'id', type: String, description: 'Plugin id', example: 'fab8f183-7021-4a42-b429-447ee7415b93' })
   id: string;
 
-  @ApiProperty({ name: 'name', type: String, description: 'Plugin name', example: 'AWS' })
-  @Column()
+  @Column({ type: 'varchar', nullable: false })
   name: string;
 
-  @Column()
-  @ApiProperty({ name: 'type', type: String, description: 'Plugin type', example: 'aws' })
+  @Column({ type: 'varchar', nullable: false })
   type: string;
 
-  @Column()
-  @ApiProperty({ name: 'description', type: String, description: 'Plugin description', example: 'Plugin AWS' })
+  @Column({ type: 'varchar', nullable: false })
   description: string;
 
-  @Column()
-  @ApiProperty({ name: 'instruction', type: String, format: 'markdown', example: pluginInstructionExample })
+  @Column({ type: 'varchar', nullable: false, default: 'No instruction' })
   instruction: string;
 
-  @Column()
-  @ApiProperty({ name: 'category', type: String, description: 'Plugin category', example: 'cloud' })
+  @Column({ type: 'varchar', nullable: false })
   category: string;
 
   @JoinColumn()
   @OneToMany(() => PluginToMetricEntity, (pluginToMetric: PluginToMetricEntity) => pluginToMetric.plugin)
-  @ApiProperty({
-    name: 'pluginToMetrics',
-    type: PluginToMetricEntity,
-    isArray: true,
-    description: 'Plugin available metrics'
-  })
   pluginToMetrics: PluginToMetricEntity[];
 
   @JoinColumn()
   @OneToMany(() => MetricEntity, (metric: MetricEntity) => metric.plugin)
-  @ApiProperty({ name: 'plugin', type: MetricEntity, description: 'Metric plugins' })
   metrics: MetricEntity;
 
   @Column({ type: 'varchar', nullable: false })
-  @ApiProperty({
-    name: 'credentialType',
-    type: String,
-    description: 'Plugin credentialType',
-    example: 'aws'
-  })
   credentialType: string;
 
-  @Column({ type: 'varchar', nullable: false })
-  @ApiProperty({
-    name: 'iconUrl',
-    type: String,
-    description: 'Plugin iconUrl',
-    example: 'https://www.google.com/images/branding/googleiconUrl/1x/googleiconUrl_color_272x92dp.png'
-  })
+  @Column({ type: 'varchar', nullable: true })
   iconUrl: string;
 
   @CreateDateColumn()
-  @ApiProperty({ name: 'createdAt', type: Date, description: 'Plugin creation date', example: '2023-01-01T00:00:00.000Z' })
   createdAt: Date;
 }
---
+++ data/migrations/1693085834707-SeedDefaultDatabase.ts
@@ -0,0 +1,162 @@
+import { randomUUID } from 'node:crypto';
+import { MigrationInterface, ObjectLiteral, QueryRunner } from 'typeorm';
+import { EntityTarget } from 'typeorm/common/EntityTarget';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+
+import { AlertEntity } from '../../apps/api/src/infrastructure/database/entities/alert.entity';
+import { CredentialEntity } from '../../apps/api/src/infrastructure/database/entities/credential.entity';
+import { MetricEntity } from '../../apps/api/src/infrastructure/database/entities/metric.entity';
+import { PluginEntity } from '../../apps/api/src/infrastructure/database/entities/plugin.entity';
+import { PluginToMetricEntity } from '../../apps/api/src/infrastructure/database/entities/plugin_to_metric.entity';
+
+export class SeedDefaultDatabase1693085834707 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    const pluginIds = {
+      aws: randomUUID(),
+      sql: randomUUID(),
+      github: randomUUID(),
+      apiHealthCheck: randomUUID()
+    };
+
+    const metricIds = {
+      apiHealthCheck: randomUUID(),
+      awsEc2SingleInstanceUsage: randomUUID(),
+      awsEc2MultipleInstancesUsage: randomUUID(),
+      githubLastIssues: randomUUID()
+    };
+
+    const pluginToMetricIds = {
+      pingApi: randomUUID()
+    };
+
+    const plugins = [
+      {
+        id: pluginIds.aws,
+        name: 'AWS',
+        type: 'aws',
+        description: 'Amazon Web Services Plugin',
+        instruction: '...waiting for instruction...',
+        category: 'cloud',
+        credentialType: 'aws',
+        iconUrl: ''
+      },
+      {
+        id: pluginIds.sql,
+        name: 'SQL Database',
+        type: 'sql_database',
+        description: 'SQL Database Plugin',
+        instruction: '...waiting for instruction...',
+        category: 'db',
+        credentialType: 'dbConnection'
+      },
+      {
+        id: pluginIds.github,
+        name: 'Github',
+        type: 'github',
+        description: 'Github Plugin',
+        instruction: '...waiting for instruction...',
+        category: 'versionning',
+        credentialType: 'github',
+        iconUrl: ''
+      },
+      {
+        id: pluginIds.apiHealthCheck,
+        name: 'API Health Check',
+        type: 'api_endpoint',
+        description: 'API Health Check Plugin',
+        instruction: '...waiting for instruction...',
+        category: 'api',
+        credentialType: 'apiEndpoint',
+        iconUrl: ''
+      }
+    ];
+    const metrics = [
+      {
+        id: metricIds.apiHealthCheck,
+        type: 'api-endpoint-health-check',
+        name: 'Ping Api',
+        pluginId: pluginIds.apiHealthCheck,
+        isNotifiable: true
+      },
+      {
+        id: metricIds.awsEc2SingleInstanceUsage,
+        type: 'aws-ec2-single-instance-usage',
+        name: 'AWS EC2 instance cost',
+        pluginId: pluginIds.aws,
+        isNotifiable: true
+      },
+      {
+        id: metricIds.awsEc2MultipleInstancesUsage,
+        type: 'aws-ec2-multiple-instances-usage',
+        name: 'AWS EC2 multiple instance cost',
+        pluginId: pluginIds.aws,
+        isNotifiable: false
+      },
+      {
+        id: metricIds.githubLastIssues,
+        type: 'github-last-issues',
+        name: 'Github last ussues',
+        pluginId: pluginIds.github,
+        isNotifiable: false
+      }
+    ];
+    const credentials = [
+      {
+        id: randomUUID(),
+        pluginId: pluginIds.apiHealthCheck,
+        type: 'apiEndpoint',
+        value: 'eyJhcGlFbmRwb2ludCI6ICJodHRwczovL2pzb25wbGFjZWhvbGRlci50eXBpY29kZS5jb20vdXNlcnMifQo='
+      },
+      {
+        id: randomUUID(),
+        pluginId: pluginIds.github,
+        type: 'github',
+        value:
+          'eyJhY2Nlc3NUb2tlbiI6ImdpdGh1Yl9wYXRfMTFBS1RYRFBBMHhVUGUzbFRUOTFXNl9CMXJzd1hwRGJCU01NTVZUUmdESE1PTUFVTHMwSkV3NlZpaHlYVjd3MFd1V01NTEZHUjRQZDNHVnA3UCIsIm93bmVyIjoibWV0cmlrdWJlIiwicmVwbyI6ImFwcCJ9'
+      }
+    ];
+    const pluginToMetrics = [
+      {
+        id: pluginToMetricIds.pingApi,
+        pluginId: pluginIds.apiHealthCheck,
+        metricId: metricIds.apiHealthCheck,
+        isActive: true,
+        resourceId: '',
+        name: "Ping l'api jsonplaceholder",
+        description: null
+      }
+    ];
+    const alerts = [
+      {
+        id: '5607a60c-1dc9-455e-817a-59c3f82a176b',
+        pluginToMetricId: pluginToMetricIds.pingApi,
+        label: 'API Response Time > 50ms',
+        triggered: false,
+        isActive: true,
+        condition: {
+          field: 'value',
+          operator: 'gte',
+          threshold: 50
+        }
+      }
+    ];
+
+    await this.execute(queryRunner, PluginEntity, plugins);
+    await this.execute(queryRunner, MetricEntity, metrics);
+    await this.execute(queryRunner, CredentialEntity, credentials);
+    await this.execute(queryRunner, PluginToMetricEntity, pluginToMetrics);
+    await this.execute(queryRunner, AlertEntity, alerts);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DELETE FROM alert WHERE id');
+    await queryRunner.query('DELETE FROM credential WHERE id');
+    await queryRunner.query('DELETE FROM plugin_to_metric WHERE id');
+    await queryRunner.query('DELETE FROM metric WHERE id');
+    await queryRunner.query('DELETE FROM plugin WHERE id');
+  }
+
+  execute(queryRunner: QueryRunner, entity: EntityTarget<ObjectLiteral>, values: QueryDeepPartialEntity<ObjectLiteral> | QueryDeepPartialEntity<ObjectLiteral>[]): Promise<unknown> {
+    return queryRunner.manager.createQueryBuilder().insert().into(entity).values(values).execute();
+  }
+}
