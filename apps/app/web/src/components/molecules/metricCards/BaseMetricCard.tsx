5f5846fc7abb455af2735ec89600c5ecdcbec5e4
Jessy <c.j91350@gmail.com>
2023-08-27 12:29:35 +0200

    refacto : modal

--- apps/app/web/src/components/molecules/metricCards/BaseMetricCard.tsx
+++ apps/app/web/src/components/molecules/metricCards/BaseMetricCard.tsx
@@ -2,9 +2,7 @@ import { DashboardContext } from '../../../contexts/Dashboard.context'
 import styled from '@emotion/styled'
 import { ActiveMetricModel } from '@metrikube/core'
 import MoreVertIcon from '@mui/icons-material/MoreVert'
-import NotificationsActiveOutlinedIcon from '@mui/icons-material/NotificationsActiveOutlined'
-import NotificationsOffOutlinedIcon from '@mui/icons-material/NotificationsOffOutlined'
-import { Card, CardHeader, IconButton, CardContent, Menu, MenuItem, Avatar } from '@mui/material'
+import { Card, CardHeader, IconButton, CardContent, Menu, MenuItem } from '@mui/material'
 import React, { Dispatch, PropsWithChildren, SetStateAction, useContext, useState } from 'react'
 
 interface Props {
@@ -41,34 +39,16 @@ const BaseMetricCard = ({
     handleClose()
   }
 
-  const toggleNotification = (isNotificationActivated: boolean) => {
-    const newNotificationStatus = !isNotificationActivated
-    console.log(newNotificationStatus)
-  }
-
-  const imageUrl = new URL(`/src/assets/img/${activeMetric.plugin!.type}.png`, import.meta.url).href
+  const imageUrl = new URL(`/src/assets/img/${activeMetric.plugin.type}.png`, import.meta.url).href
 
   return (
     <Card>
       <CardHeader
         title={activeMetric.metric.name}
         subheader="September 14, 2016"
-        avatar={<Logo alt={activeMetric.plugin!.name} src={imageUrl} />}
+        avatar={<Logo alt={activeMetric.plugin.name} src={imageUrl} />}
         action={
           <IconsActionContainer>
-            {activeMetric.metric.isNotifiable ? (
-              <IconButton
-                onClick={() => toggleNotification(activeMetric.metric.isNotifiable)}
-                aria-label="alert-on">
-                <NotificationsActiveOutlinedIcon />
-              </IconButton>
-            ) : (
-              <IconButton
-                onClick={() => toggleNotification(activeMetric.metric.isNotifiable)}
-                aria-label="alert-off">
-                <NotificationsOffOutlinedIcon />
-              </IconButton>
-            )}
             <div>
               <IconButton onClick={handleClick}>
                 <MoreVertIcon />
--- apps/app/web/src/components/organisms/modals/ConfirmDeletion.modal.tsx
+++ apps/app/web/src/components/organisms/modals/ConfirmDeletion.modal.tsx
@@ -1,19 +1,51 @@
 import { DashboardContext } from '../../../contexts/Dashboard.context'
-import { Dialog, DialogContent, DialogTitle, IconButton } from '@mui/material'
-import React, { Dispatch, SetStateAction, useContext } from 'react'
+import { Dialog, DialogContent, DialogTitle, DialogActions, TextField, Button } from '@mui/material'
+import React, { Dispatch, SetStateAction, useContext, useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 interface Props {
   open: boolean
   setOpenModal: Dispatch<SetStateAction<boolean>>
 }
 
+type ConfirmDeletionForm = {
+  confirmDeletion: string
+}
+
 const ConfirmDeletionModal = ({ open, setOpenModal }: Props) => {
   const { selectedActiveMetric } = useContext(DashboardContext)
+  const {
+    register,
+    getValues,
+    formState: { errors }
+  } = useForm<ConfirmDeletionForm>()
+
+  useEffect(() => {
+    console.log(getValues().confirmDeletion)
+  }, [getValues().confirmDeletion])
 
   return (
     <Dialog open={open} onClose={() => setOpenModal(false)}>
       <DialogTitle>Supprimer : {selectedActiveMetric?.metric.name}</DialogTitle>
-      <DialogContent></DialogContent>
+      <DialogContent>
+        <form>
+          <p>Pour confirmer la suppression</p>
+          <TextField
+            fullWidth
+            label="SUPPRIMER"
+            helperText="Vous devez écrire SUPPRIMER"
+            variant="outlined"
+            size="small"
+            {...register('confirmDeletion')}
+          />
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => setOpenModal(false)}>Cancel</Button>
+        <Button disabled={false} variant="outlined" color="error">
+          SUPPRIMER
+        </Button>
+      </DialogActions>
     </Dialog>
   )
 }
