c900f34321cd5214f38cd8494afecea40ef07e2c
Yahia <yahia.lamri@outlook.fr>
2023-09-02 18:24:53 +0200

    chore(database-plugin): update types

--- apps/api/src/domain/interfaces/use-cases/credential.use-case.interface.ts
+++ apps/api/src/domain/interfaces/use-cases/credential.use-case.interface.ts
@@ -1,15 +1,13 @@
 import { Connection } from 'mysql2/promise';
+
+import { ApiDatabaseLastAverageQueriesByHour, ApiDatabaseSize, ApiDatabaseSlowQueries } from '@metrikube/common';
+
 import { CredentialEntity } from '../../../infrastructure/database/entities/credential.entity';
 import { Credential } from '../../models/credential.model';
-import { ApiDatabaseSize, ApiDatabaseSlowQueries, ApiDatabaseLastAverageQueriesByHour } from '@metrikube/common';
-
 
 export interface CredentialUseCaseInterface {
   insertCredentialForPlugin(pluginId: string, credential: Credential): Promise<CredentialEntity>;
   getNbQueries(pluginId: string): Promise<ApiDatabaseLastAverageQueriesByHour>;
   getDbSize(pluginId: string): Promise<ApiDatabaseSize>;
-  getSlowQuery(pluginId: string): Promise<ApiDatabaseSlowQueries>;
-
-
-
+  getSlowQuery(pluginId: string): Promise<ApiDatabaseSlowQueries[]>;
 }
