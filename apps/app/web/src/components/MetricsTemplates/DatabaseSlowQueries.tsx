54b0a9e0f5269e4c37b4dea142922dae21f7f514
Diarit <diarit-94@hotmail.fr>
2023-09-08 13:50:58 +0200

    style(metric templates): Some customizations

--- apps/app/web/src/components/MetricsTemplates/DatabaseSlowQueries.tsx
+++ apps/app/web/src/components/MetricsTemplates/DatabaseSlowQueries.tsx
@@ -1,7 +1,8 @@
-
+import SqlCodeBlock from '../molecules/SqlCodeBlock'
 import ListResource from '../molecules/WidgetsGenericTemplates/TableWidget'
 import { ActiveMetricModel, formatAsCurrency } from '@metrikube/core'
-import { TableCell, TableRow } from '@mui/material'
+import { TableCell, TableRow, Button, Dialog, DialogContent } from '@mui/material'
+import dayjs from 'dayjs'
 import React from 'react'
 
 interface Props {
@@ -9,18 +10,40 @@ interface Props {
 }
 
 export const DatabaseSlowQueries = ({ metric }: Props) => {
+  const [isSqlModalOpen, setIsSqlModalOpen] = React.useState(false)
+  const [queryToDisplay, setQueryToDisplay] = React.useState('')
+
+  const showSqlModal = (query: string) => {
+    setQueryToDisplay(query)
+    setIsSqlModalOpen(true)
+  }
+
   return (
-    <ListResource
-      tableHead={['Requête', "Temps d'éxec (en seconde)", 'Date'].map((column, index) => (
-        <TableCell key={index}>{column}</TableCell>
-      ))}
-      tableBody={metric.data.map((slowQuery, index) => (
-        <TableRow key={index}>
-          <TableCell>{slowQuery.query}</TableCell>
-          <TableCell>{slowQuery.executionTime}</TableCell>
-          <TableCell>{slowQuery.date}</TableCell>
-        </TableRow>
-      ))}
-    />
+    <>
+      <ListResource
+        tableHead={['Requête', "Temps d'éxec (en seconde)", 'Date'].map((column, index) => (
+          <TableCell key={index}>{column}</TableCell>
+        ))}
+        tableBody={metric.data.map((slowQuery, index) => (
+          <TableRow key={index}>
+            <TableCell>
+              <Button
+                variant="outlined"
+                disableRipple
+                onClick={() => showSqlModal(slowQuery.query)}>
+                Voir la requête
+              </Button>
+            </TableCell>
+            <TableCell>{slowQuery.executionTime}</TableCell>
+            <TableCell>{dayjs(slowQuery.date).format('D MMM YYYY')}</TableCell>
+          </TableRow>
+        ))}
+      />
+      <Dialog open={isSqlModalOpen} onClose={() => setIsSqlModalOpen(false)}>
+        <DialogContent>
+          <SqlCodeBlock code={queryToDisplay.replace('@', '')} />
+        </DialogContent>
+      </Dialog>
+    </>
   )
 }
