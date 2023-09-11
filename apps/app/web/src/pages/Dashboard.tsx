0ef8724df0d4c2958fb2aff4fd6bcefa7d907ec8
Diarit <diarit-94@hotmail.fr>
2023-09-11 09:22:22 +0200

    fix(typo): Dashboard JSX

--- apps/app/web/src/pages/Dashboard.tsx
+++ apps/app/web/src/pages/Dashboard.tsx
@@ -37,7 +37,7 @@ const Dashboard = () => {
   }
 
   return (
-    <DefaultLayout>
+    <>
       <StyledHeader>
         <Brand>
           <img src={MetrikubeLogo} style={{ height: '50px' }} />
@@ -55,7 +55,7 @@ const Dashboard = () => {
           </Button>
         </div>
       </StyledHeader>
-    <DefaultLayout>
+      <DefaultLayout>
         {isFetching ? (
           <Box
             sx={{
