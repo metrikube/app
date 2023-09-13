5b3f9bbe06ea7f191684f42d63e9c23473587297
Yahia <yahia.lamri@outlook.fr>
2023-09-13 21:51:40 +0200

    feat(encryption): intialize encryption service

---
+++ apps/api/src/infrastructure/services/common/encryption.service.ts
@@ -0,0 +1,64 @@
+import { createCipheriv, createDecipheriv, createHash, randomBytes } from 'node:crypto';
+
+import { Injectable } from '@nestjs/common';
+
+type DynamicKeyValue = { [key: string]: string };
+
+@Injectable()
+export class EncryptionService {
+  algorithm = 'aes-256-ctr';
+
+  constructor(private masterPassword: string) {}
+
+  encrypt(value: string): string {
+    const iv = randomBytes(16);
+
+    const cipher = createCipheriv(this.algorithm, this.getKey(), iv);
+
+    const encrypted = Buffer.concat([cipher.update(value), cipher.final()]);
+
+    return this.encode64({
+      iv: iv.toString('hex'),
+      encryptedData: encrypted.toString('hex')
+    });
+  }
+
+  decrypt(hash: string): string {
+    const { iv: deIv, encryptedData } = this.decode64<{
+      iv: string;
+      encryptedData: string;
+    }>(hash);
+
+    const decipher = createDecipheriv(this.algorithm, this.getKey(), Buffer.from(deIv, 'hex'));
+
+    const encryptedHex = Buffer.from(encryptedData, 'hex');
+    const decrpyted = Buffer.concat([decipher.update(encryptedHex), decipher.final()]);
+
+    return decrpyted.toString();
+  }
+
+  private getKey(): Buffer {
+    const newHash = createHash('sha256');
+    const hash = newHash.update(this.masterPassword).digest('hex');
+
+    return Buffer.from(hash, 'hex');
+  }
+
+  /**
+   * encode base64 data
+   * @param args
+   */
+  private encode64(args: DynamicKeyValue): string {
+    const str = JSON.stringify(args);
+    return Buffer.from(str).toString('base64');
+  }
+
+  /**
+   * decode base64 data
+   * @param token
+   */
+  private decode64<T extends DynamicKeyValue>(token: string): T {
+    const buf = Buffer.from(token, 'base64').toString('utf8');
+    return JSON.parse(buf);
+  }
+}
