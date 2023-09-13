07b3fca0784e503889759a7f78d1736b774cc4e9
jessyv96 <c.j91350@gmail.com>
2023-09-13 13:01:25 +0200

    Delete apps/app/web/tests/test-1.spec.ts

--- apps/app/web/tests/test-1.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('test', async ({ page }) => {
-  await page.goto('http://localhost:4200/');
-  await page.getByRole('banner').getByRole('button', { name: 'Ajouter un widget' }).click();
-  await page.getByRole('button', { name: 'Ajouter un widget' }).nth(1).click();
-});
