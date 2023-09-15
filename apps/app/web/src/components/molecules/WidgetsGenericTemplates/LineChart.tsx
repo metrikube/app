853ed8204c1d02735f2d6a6ac673b64cabea455e
Diarit <diarit-94@hotmail.fr>
2023-09-15 12:28:07 +0200

    style(widgets): Multiple UI changes

--- apps/app/web/src/components/molecules/WidgetsGenericTemplates/LineChart.tsx
+++ apps/app/web/src/components/molecules/WidgetsGenericTemplates/LineChart.tsx
@@ -19,11 +19,16 @@ type Props = {
 }
 
 const LineChart = ({ labels, data }: Props) => {
+  const getThreshold = (num: number): number => {
+    if (num < 10) return 1
+    return Math.pow(10, Math.floor(Math.log10(num)))
+  }
+
   const options = {
     responsive: true,
     plugins: {
       legend: {
-        position: 'top' as const
+        display: false
       }
     },
     scales: {
@@ -31,11 +36,17 @@ const LineChart = ({ labels, data }: Props) => {
         beginAtZero: true,
         suggestedMax: 20,
         ticks: {
-          stepSize: 1
+          stepSize: getThreshold(Math.max(...data))
+        }
+      },
+      x: {
+        grid: {
+          display: false
         }
       }
     }
   }
+
   return (
     <Line
       options={options}
@@ -46,7 +57,8 @@ const LineChart = ({ labels, data }: Props) => {
             label: 'Nombre de requêtes/heure',
             data,
             borderColor: 'rgb(53, 162, 235)',
-            backgroundColor: 'rgba(53, 162, 235, 0.5)'
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
+            tension: 0.5
           }
         ]
       }}
