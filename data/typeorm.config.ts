3d7b7d07d8086a9b2a523e6cfef1622751fe3672
Yahia <yahia.lamri@outlook.fr>
2023-10-01 16:02:05 +0200

    feat(migrations): fix orm migrations config

--- data/typeorm.config.ts
+++ data/typeorm.config.ts
@@ -1,13 +1,10 @@
 import { DataSource } from 'typeorm';
 
-const entititesPath = process.env.NODE_ENV === 'production' ? 'dist-migrations/**/entities/*.entity.js' : 'apps/**/entities/*.entity.ts';
-const migrationsPath = process.env.NODE_ENV === 'production' ? 'dist-migrations/**/migrations/*.js' : 'data/**/migrations/*.ts';
-
 export default new DataSource({
   type: 'better-sqlite3',
   database: 'data/db',
-  entities: [entititesPath],
+  entities: ['dist-migrations/**/entities/*.entity.js'],
   migrationsTableName: '_migrations',
-  migrations: [migrationsPath],
+  migrations: ['dist-migrations/data/migrations/*.js'],
   migrationsTransactionMode: 'all'
 });
