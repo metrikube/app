5c1d21845cb1b1898553f9da651bf0369b4e2a20
Yahia <yahia.lamri@outlook.fr>
2023-10-01 15:02:00 +0200

    chore(migrations): fix path for deployment

--- data/typeorm.config.ts
+++ data/typeorm.config.ts
@@ -1,10 +1,13 @@
 import { DataSource } from 'typeorm';
 
+const entititesPath = process.env.NODE_ENV === 'production' ? 'dist-migrations/**/entities/*.entity.js' : 'apps/**/entities/*.entity.ts';
+const migrationsPath = process.env.NODE_ENV === 'production' ? 'dist-migrations/**/migrations/*.js' : 'data/**/migrations/*.ts';
+
 export default new DataSource({
   type: 'better-sqlite3',
   database: 'data/db',
-  entities: ['dist-migrations/**/entities/*.entity.js', 'apps/**/entities/*.entity.ts'],
+  entities: [entititesPath],
   migrationsTableName: '_migrations',
-  migrations: ['dist-migrations/**/migrations/*.js', 'data/**/migrations/*.ts'],
+  migrations: [migrationsPath],
   migrationsTransactionMode: 'all'
 });
