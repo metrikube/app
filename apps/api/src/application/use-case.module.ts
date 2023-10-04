eddb3bc07dcccbd043bdad550f8804545db41e7f
Yahia <yahia.lamri@outlook.fr>
2023-10-04 22:58:47 +0200

    chore(infra): move usecase dependencies to infrastructure module and clean dependencies import order

--- apps/api/src/application/use-case.module.ts
+++ apps/api/src/application/use-case.module.ts
@@ -1,69 +1,24 @@
 import { Module, Provider } from '@nestjs/common';
 
-import { ApiMonitoringModule, ApiMonitoringService } from '@metrikube/api-monitoring';
-import { AWSService, AwsPluginModule } from '@metrikube/aws-plugin';
-import { DbAnalyticsPluginModule, DbAnalyticsPluginService } from '@metrikube/db-analytics-plugin';
-import { GithubPluginModule, GithubService } from '@metrikube/github-plugin';
-
-import { AlertRepositoryImpl } from '../infrastructure/database/repositories/alert.repository';
-import { CredentialRepositoryImpl } from '../infrastructure/database/repositories/credential.repository';
-import { MetricRepositoryImpl } from '../infrastructure/database/repositories/metric.repository';
-import { PluginRepositoryImpl } from '../infrastructure/database/repositories/plugin.repository';
-import { WidgetRepositoryImpl } from '../infrastructure/database/repositories/wiget.repository';
 import { DiTokens } from '../infrastructure/di/tokens';
 import { InfrastructureModule } from '../infrastructure/infrastructure.module';
-import { EncryptionService } from '../infrastructure/services/common/encryption.service';
-import { PluginResolverService } from '../infrastructure/services/common/plugin-resolver.service';
-import { NotificationService } from '../infrastructure/services/notification/notification.service';
-import { SchedulerService } from '../infrastructure/services/scheduler/scheduler.service';
 import { AlertUseCase } from './use-cases/alert/alert.use-case';
 import { CredentialUseCase } from './use-cases/credential/credential.use-case';
 import { DashboardUseCase } from './use-cases/dashboard/dashboard.use-case';
 import { PluginUseCase } from './use-cases/plugin/plugin.use-case';
 import { WidgetUsecase } from './use-cases/widget/widget.usecase';
 
-const providers: Provider[] = [
-  // | Use Cases
-  // |
-  // |
-  // ---------------------------------------------------------------------------
+const useCaseProviders: Provider[] = [
   { provide: DiTokens.AlertUseCaseToken, useClass: AlertUseCase },
   { provide: DiTokens.PluginUseCaseToken, useClass: PluginUseCase },
   { provide: DiTokens.DashboardUseCaseToken, useClass: DashboardUseCase },
   { provide: DiTokens.CredentialUseCaseToken, useClass: CredentialUseCase },
-  { provide: DiTokens.WidgetUseCaseToken, useClass: WidgetUsecase },
-  // | Repositories
-  // |
-  // |
-  // ---------------------------------------------------------------------------
-  { provide: DiTokens.AlertRepositoryToken, useClass: AlertRepositoryImpl },
-  { provide: DiTokens.CredentialRepositoryToken, useClass: CredentialRepositoryImpl },
-  { provide: DiTokens.PluginRepositoryToken, useClass: PluginRepositoryImpl },
-  { provide: DiTokens.MetricRepositoryToken, useClass: MetricRepositoryImpl },
-  { provide: DiTokens.WidgetRepositoryToken, useClass: WidgetRepositoryImpl },
-  // | Plugin Services
-  // |
-  // |
-  // ---------------------------------------------------------------------------
-  { provide: DiTokens.DbAnalyticsPluginServiceToken, useClass: DbAnalyticsPluginService },
-  { provide: DiTokens.ApiMonitoringToken, useClass: ApiMonitoringService },
-  { provide: DiTokens.AWSServiceToken, useClass: AWSService },
-  { provide: DiTokens.GithubServiceToken, useClass: GithubService },
-  // | Application Services
-  // |
-  // |
-  // ---------------------------------------------------------------------------
-  { provide: DiTokens.EncryptionService, useFactory: () => new EncryptionService(process.env.MASTER_PASSWORD) },
-  { provide: DiTokens.Mailer, useClass: NotificationService },
-  { provide: DiTokens.PluginResolver, useClass: PluginResolverService },
-  { provide: DiTokens.Scheduler, useClass: SchedulerService }
+  { provide: DiTokens.WidgetUseCaseToken, useClass: WidgetUsecase }
 ];
 
-const pluginsModules = [ApiMonitoringModule, AwsPluginModule, DbAnalyticsPluginModule, GithubPluginModule];
-
 @Module({
-  imports: [InfrastructureModule, ...pluginsModules],
-  providers,
-  exports: [...providers]
+  imports: [InfrastructureModule],
+  providers: [...useCaseProviders],
+  exports: [...useCaseProviders]
 })
 export class UseCaseModule {}
