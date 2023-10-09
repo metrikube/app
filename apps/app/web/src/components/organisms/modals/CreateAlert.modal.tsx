fc82e44e54d2f8879f3764cbf427414d980e53d6
Diarit <diarit-94@hotmail.fr>
2023-10-09 21:17:21 +0200

    fix(alerts): Display default alert form

--- apps/app/web/src/components/organisms/modals/CreateAlert.modal.tsx
+++ apps/app/web/src/components/organisms/modals/CreateAlert.modal.tsx
@@ -54,7 +54,7 @@ const CreateAlertModal = ({ open, setOpenModal, widget }: Props) => {
       <DialogContent>
         <FormProvider {...methods}>
           <form onSubmit={methods.handleSubmit(onSubmit)}>
-            <AlertCreationForm alertFields={alertFields} noDefaultForm />
+            <AlertCreationForm alertFields={alertFields} />
             <Button
               type="submit"
               variant="contained"
